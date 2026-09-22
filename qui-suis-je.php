618c373339df5863a0a7c3181a28eed2e7ec7c2c
Sophie Diaz <diazsophie91@gmail.com>
2026-09-22 21:40:30 +0200

    Reflet lumineux amélioré sur le texte de la citation pour un effet plus subtil et élégant.

--- qui-suis-je.php
+++ qui-suis-je.php
@@ -367,10 +367,12 @@ Template Name: Qui suis-je ?
 
 		<div class="qui-suis-je-citation-interieur">
 
-			<span class="qui-suis-je-citation-guillemets">« </span>
-			<span class="qui-suis-je-citation-texte">La vie, ce n’est pas d’attendre que les orages passent, c’est d’apprendre comment danser sous la pluie.</span>
-			<span class="qui-suis-je-citation-guillemets"> »</span>
-			<span class="qui-suis-je-citation-auteur">- Sénèque</span>
+			<span class="qui-suis-je-citation-lumineuse">
+				<span class="qui-suis-je-citation-guillemets">« </span>
+				<span class="qui-suis-je-citation-texte">La vie, ce n’est pas d’attendre que les orages passent, c’est d’apprendre comment danser sous la pluie.</span>
+				<span class="qui-suis-je-citation-guillemets"> »</span>
+				<span class="qui-suis-je-citation-auteur">- Sénèque</span>
+			</span>
 
 		</div>
 
